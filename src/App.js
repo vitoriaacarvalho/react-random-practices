6585319dc73da228b62330632c6b2929113ecf3d
vitoriaacarvalho <vitoriabcarvalhocorrea@gmail.com>
2022-09-19 19:00:26 -0300

    lifecycle methods pt1

--- src/App.js
+++ src/App.js
@@ -1,40 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
-class App extends Component {
-    state={
-      posts:[
-        {
-          id:1,
-          title: 'o título 1',
-          body:'o corpo 1'
-        },
-        {
-          id: 2,
-          title: 'o título 2',
-          body: 'o corpo 2'
-        },
-        {
-          id: 3,
-          title:'o título 3',
-          body: 'o corpo 3'
-        },
-      ]
-    };
-    render(){
-      const{posts}=this.state;
-      return(
-        <div className="App">
-          {posts.map( post => (
-            <div key={post.id}>
-                <h1>{post.title}</h1>
-                <h2>{post.body}</h2>
-            </div>
-          ))}          
-        </div>
-      )
-    };
-}
+class App extends Component{
+  state={
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'título 1',
+        body: 'corpo 1'
+      },
+      {
+        id: 2,
+        title: 'título 2',
+        body: 'corpo 2'
+      },
+      {
+        id: 3,
+        title: 'título 3',
+        body: 'corpo 3'
+      },
+    ]
+  };
+  timeoutUpdate = null;
 
+  componentDidMount(){
+    this.handleTimeout();
+  }
+
+  componentDidUpdate(){
+    this.handleTimeout();
+  }
+
+  componentWillUnmount(){
+    clearTimeout(this.timeoutUpdate);
+  }
+
+  handleTimeout(){
+    const { posts , counter } =this.state;
+    posts[0].title='o título mudou';
+
+    this.timeoutUpdate=setTimeout(() => {
+      this.setState({ posts, counter: counter+1});
+    },1000);
+  }
+
+  render(){
+    const { posts, counter } = this.state;
+
+    return(
+      <div className="App">
+        <h1>{counter}</h1>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+} 
 export default App;
